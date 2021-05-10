45418e2cb7dc230fa69365d90529de7af5a77d07
hoàng huy <huyhoang121296@gmail.com>
2021-05-10 20:09:49 +0700

    Create project

---
+++ Q_Network/Q_Network/Controllers/DiscardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Q_Network.Controllers
+{
+    public class DiscardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
